13a814ad4abf327a44e37c8d78d58ea46e37d041
luis <ing.luis.snchz@gmail.com>
2024-06-17 22:20:23 -0600

    Commit initial

---
+++ exampleAPI2/Controllers/UserController.cs
@@ -0,0 +1,100 @@
+﻿using exampleAPI2.Models;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace exampleAPI2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        public static readonly IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+        [HttpGet]
+        [Route("/User/detail")]
+        public ActionResult<User> Get(int idUser)
+        {
+            try
+            {
+                using (MySqlConnection conex = new(config.GetConnectionString("MYSQL_FINANCE")))
+                {
+                    conex.Open();
+                    MySqlCommand comando = conex.CreateCommand();
+                    comando.CommandText = "select * from financedb.usuario where id = ?id";
+                    comando.Parameters.AddWithValue("id", idUser);
+                    MySqlDataReader reader = comando.ExecuteReader();
+                    User userData = new();
+
+                    while (reader.Read())
+                    {
+                        userData.id = (int)reader["id"];
+                        userData.name = (string)reader["nombreUsuario"];
+                        userData.email = (string)reader["email"];
+                        userData.fechaCreacion = (DateTime)reader["fechaCreacion"];
+                        try
+                        {
+                            userData.imgURL = (string)reader["imgURL"];
+                        }
+                        catch
+                        {
+                            userData.imgURL = null;
+                        }
+                        userData.activo = (bool)reader["activo"];
+                    }
+                    conex.Close();
+                    Console.WriteLine("Hooola mundo recargado");
+                    return userData;
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine("Respuesta de consulta ", ex);
+                throw;
+            }
+        }
+
+        [HttpGet]
+        [Route("/User/list")]
+        public ActionResult<IEnumerable<User>> GetUserList()
+        {
+            try
+            {
+                using (MySqlConnection conex = new(config.GetConnectionString("MYSQL_FINANCE")))
+                {
+                    conex.Open();
+                    MySqlCommand comando = conex.CreateCommand();
+                    comando.CommandText = "select * from financedb.usuario where activo = 1";
+                    MySqlDataReader reader = comando.ExecuteReader();
+                    List<User> userDataList = new List<User>();
+
+                    while (reader.Read())
+                    {
+                        User userData = new User();
+                        userData.id = (int)reader["id"];
+                        userData.name = (string)reader["nombreUsuario"];
+                        userData.email = (string)reader["email"];
+                        userData.fechaCreacion = (DateTime)reader["fechaCreacion"];
+                        try
+                        {
+                            userData.imgURL = (string)reader["imgURL"];
+                        }
+                        catch
+                        {
+                            userData.imgURL = null;
+                        }
+                        userData.activo = (bool)reader["activo"];
+                        userDataList.Add(userData);
+                    }
+                    conex.Close();
+                    Console.WriteLine("data " + userDataList);
+                    return userDataList;
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine("Respuesta de consulta ", ex);
+                throw;
+            }
+        }
+    }
+}
---
+++ exampleAPI2/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace exampleAPI2.Models
+{
+    public class User
+    {
+        public int? id { get; set; } = null;
+        public string name { get; set; } = "";
+        public string email { get; set; } = "";
+        public DateTime? fechaCreacion { get; set; } = null;
+        public string? imgURL { get; set; } = "";
+        public bool activo { get; set; } = false;
+    }
+
+    public class ResponseUser
+    {
+        public string? textResponse { get; set; } = null;
+        public object? data { get; set; } = null;
+    }
+}
